eff5963e7e2127b1adfe0da018faf039ff0796dc
pratikShewane369 <mohanshewane@gmail.com>
2026-07-23 22:43:16 +0530

    Updated Script and Env Port Listening instead of hardcode

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URI)
 })
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App is listening to port ${PORT}`);
