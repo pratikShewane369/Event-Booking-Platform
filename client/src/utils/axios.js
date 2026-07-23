aae5a282583ef56a3094d2e8335e6c07f54a5fcb
Pratik Shewane <mohanshewane@gmail.com>
2026-07-23 23:17:38 +0530

    Change API base URL to production server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://localhost:5000/api',
+    baseURL : 'https://eventora-backend-mjr2.onrender.com',
     headers : {
         'Content-Type' : 'application/json',
     },
