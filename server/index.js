d083dde213981b4f6182c4dd54f49b916cac506c
Pratik Shewane <mohanshewane@gmail.com>
2026-07-24 00:01:52 +0530

    Fix CORS origin URL format

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://eventora-frontend-murex.vercel.app/"
+    "https://eventora-frontend-murex.vercel.app"
   ],
   credentials: true,
 }));
